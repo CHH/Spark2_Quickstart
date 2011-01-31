1f4f44d0bf84fce1a3eb9ec3893f7681fd9d938a
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2011-01-31 13:36:08 +0100

    Added WEB_ROOT constant to index.php

--- webroot/index.php
+++ webroot/index.php
@@ -2,11 +2,15 @@
 
 use Spark\App, Spark\HttpRequest, Spark\HttpResponse;
 
-define("APP_ROOT",   realpath(__DIR__ . "/../"));
+define("WEB_ROOT"    __DIR__);
+define("APP_ROOT",   realpath(WEB_ROOT . "/../"));
 define("LIB",        APP_ROOT . "/lib");
 define("SPARK_PATH", LIB . "/Spark/lib");
 
+// Load the Framework
 require_once SPARK_PATH . "/Spark.php";
+
+// Bootstrap the Application
 require_once APP_ROOT   . "/app.php";
 
 Spark::run();
